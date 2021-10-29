416eb4fc672afdd51c18b1911e85941260fe681b
dinhviettoanle <le.dinh.viet.toan@gmail.com>
2021-10-29 14:54:23 +0200

    tests: add runways in database

--- tests/apis_app/app/models.py
+++ tests/apis_app/app/models.py
@@ -14,3 +14,24 @@ class Airport(models.Model):
 
     def __str__(self):
         return f"(Airport) : {self.icao}"
+
+
+class Runway(models.Model):
+    airport = models.ForeignKey('Airport', on_delete=models.CASCADE)
+    length = models.FloatField()
+    width = models.FloatField()
+    surface = models.CharField(max_length=20)
+    le_ident = models.CharField(max_length=5)
+    le_heading = models.FloatField()
+    le_latitude = models.FloatField()
+    le_longitude = models.FloatField()
+    le_altitude = models.FloatField()
+    he_ident = models.CharField(max_length=5)
+    he_heading = models.FloatField()
+    he_latitude = models.FloatField()
+    he_longitude = models.FloatField()
+    he_altitude = models.FloatField()
+
+    def __str__(self):
+        return f"(Runway) : {self.le_ident}/{self.he_ident} - {self.airport}"
+
--- tests/apis_app/init_database.py
+++ tests/apis_app/init_database.py
@@ -18,7 +18,7 @@ import pandas as pd
 import io
 from tqdm.autonotebook import tqdm
 
-from app.models import Airport
+from app.models import Airport, Runway
 
 
 class AirportLoader:
@@ -69,7 +69,7 @@ class AirportLoader:
         for chunk in tqdm(
             f.iter_content(1024),
             total=total // 1024 + 1 if total % 1024 > 0 else 0,
-            desc="Requesting @ourairports",
+            desc="Requesting airport@ourairports",
         ):
             buffer.write(chunk)
 
@@ -90,10 +90,44 @@ class AirportLoader:
 
 
 
-def import_data(airport_data):
-    return
+class RunwayLoader:
 
-def main():
+    def __init__(self):
+        self.data = self.download_ourairports()
+
+    def get_runway_data(self):
+        return self.data
+    
+    def download_ourairports(self):
+        
+        df_airportsfr24 = AirportLoader().download_fr24().drop(columns=['desc', 'municipality'])
+        s = requests.session()
+
+        f = s.get("https://ourairports.com/data/runways.csv", stream=True)
+        total = int(f.headers["Content-Length"])
+        buffer = io.BytesIO()
+        for chunk in tqdm(
+            f.iter_content(1024),
+            total=total // 1024 + 1 if total % 1024 > 0 else 0,
+            desc="Requesting runway@ourairports",
+        ):
+            buffer.write(chunk)
+
+        buffer.seek(0)
+        df_runways = pd.read_csv(buffer)
+
+        df_all = (df_airportsfr24
+            .merge(df_runways, left_on='icao', right_on='airport_ident', how='left')
+            .drop(columns=['id', 'airport_ident', 'airport_ref', 'le_displaced_threshold_ft', 'he_displaced_threshold_ft'])
+            .dropna(axis=0)
+            .reset_index()
+            )
+
+        return df_all
+
+
+
+def import_airports():
     airport_data = AirportLoader().get_airport_data()
     pbar = tqdm(total = len(airport_data), desc="Creating entries")
     list_airports = []
@@ -114,8 +148,50 @@ def main():
     pbar.close()
 
     Airport.objects.bulk_create(list_airports)
-
     print("Done !")
+
+
+def import_runways():
+    runway_data = RunwayLoader().get_runway_data()
+    pbar = tqdm(total = len(runway_data), desc="Creating entries")
+    list_runways = []
+    for i, runway in runway_data.iterrows():
+        this_airport = Airport.objects.get(icao=runway['icao'])
+        new_runway = Runway(
+            airport = this_airport,
+            length = runway['length_ft'],
+            width = runway['width_ft'],
+            surface = runway['surface'],
+            le_ident = runway['le_ident'],
+            le_heading = runway['le_heading_degT'],
+            le_latitude = runway['le_latitude_deg'],
+            le_longitude = runway['le_longitude_deg'],
+            le_altitude = runway['le_elevation_ft'],
+            he_ident = runway['he_ident'],
+            he_heading = runway['he_heading_degT'],
+            he_latitude = runway['he_latitude_deg'],
+            he_longitude = runway['he_longitude_deg'],
+            he_altitude = runway['he_elevation_ft'],
+        )
+        list_runways.append(new_runway)
+        pbar.update(1)
+    pbar.close()
+
+    Runway.objects.bulk_create(list_runways)
+    print("Done !")
+
+
+def test():
+    print(vars(Airport.objects.filter(icao='LFBO')[0]))
+    print()
+    print(Runway.objects.filter(airport__icao='LFBO'))
+    print(vars(Runway.objects.filter(airport__icao='LFBO')[0]))
+
+def main():
+    # import_airports()
+    # import_runways()
+
+    test()
 
 
 if __name__ == '__main__':
