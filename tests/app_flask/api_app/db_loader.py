f4882005732f09beab9e96b34934604c4dc96309
dinhviettoanle <le.dinh.viet.toan@gmail.com>
2021-11-05 22:06:23 +0100

    tests: add navaids and frequencies in database

--- tests/app_flask/api_app/db_loader.py
+++ tests/app_flask/api_app/db_loader.py
@@ -4,10 +4,17 @@ import io
 from tqdm.autonotebook import tqdm
 import logging as lg
 
-from .models import db, Airport, Runway
+from .models import db, Airport, Runway, Navaid, Frequency
+
+
+# =========================================================================================
+# =========================================================================================
+# LOADERS
+# =========================================================================================
+# =========================================================================================
+
 
 class AirportLoader:
-
     def __init__(self, source='fr24'):
         if source == 'fr24':
             self.data = self.download_fr24()
@@ -80,9 +87,10 @@ class AirportLoader:
         return df_airports
 
 
+# =========================================================================================
+
 
 class RunwayLoader:
-
     def __init__(self):
         self.data = self.download_ourairports()
 
@@ -117,11 +125,106 @@ class RunwayLoader:
         return df_all
 
 
+# =========================================================================================
+
+
+class NavaidLoader:
+    def __init__(self):
+        self.data = self.download_navaids()
+
+    def get_navaid_data(self):
+        return self.data
+    
+    def download_navaids(self):
+        df_airportsfr24 = AirportLoader().download_fr24().drop(columns=['desc', 'municipality'])
+        s = requests.session()
+
+        # Load countries
+        f = s.get("https://ourairports.com/data/countries.csv")
+        buffer = io.BytesIO(f.content)
+        buffer.seek(0)
+        df_countries = pd.read_csv(buffer)
+        df_countries = df_countries.rename(columns={'code' : 'iso_country', 'name' : 'country'})
+
+        # Load navaids
+        f = s.get("https://ourairports.com/data/navaids.csv", stream=True)
+        total = int(f.headers["Content-Length"])
+        buffer = io.BytesIO()
+        for chunk in tqdm(
+            f.iter_content(1024),
+            total=total // 1024 + 1 if total % 1024 > 0 else 0,
+            desc="Requesting navaid@ourairports",
+        ):
+            buffer.write(chunk)
+
+        buffer.seek(0)
+        df_navaids = pd.read_csv(buffer)
+
+        df_all = (df_navaids
+            .merge(df_countries[['iso_country', 'country']])
+            .merge(df_airportsfr24[['name', 'icao']], left_on='associated_airport', right_on='icao', how='left')
+            .drop(columns=['id', 'filename', 'iso_country', 'associated_airport',
+                            'dme_frequency_khz', 'dme_channel', 'dme_latitude_deg', 'dme_longitude_deg', 'dme_elevation_ft',
+                            'slaved_variation_deg', 'magnetic_variation_deg'])
+            .rename(columns={'name_x' : 'navaid_name', 'name_y' : 'airport_name', 'icao' : 'airport_icao'})
+            .reset_index()
+                )
+        df_all['ident'].fillna('NA', inplace=True)
+        df_all['elevation_ft'].fillna(0, inplace=True)
+        df_all = df_all.where(pd.notnull(df_all), None)
+
+
+        return df_all
+
+
+# =========================================================================================
+
+
+class FrequencyLoader:
+    def __init__(self):
+        self.data = self.download_frequencies()
+
+    def get_frequency_data(self):
+        return self.data
+    
+    def download_frequencies(self):
+        df_airportsfr24 = AirportLoader().download_fr24().drop(columns=['desc', 'municipality'])        
+        s = requests.session()
+
+        f = s.get("https://ourairports.com/data/airport-frequencies.csv", stream=True)
+        total = int(f.headers["Content-Length"])
+        buffer = io.BytesIO()
+        for chunk in tqdm(
+            f.iter_content(1024),
+            total=total // 1024 + 1 if total % 1024 > 0 else 0,
+            desc="Requesting frequency@ourairports",
+        ):
+            buffer.write(chunk)
+
+        buffer.seek(0)
+        df_frequencies = pd.read_csv(buffer)
+
+
+        df_all = (df_airportsfr24
+            .merge(df_frequencies, left_on='icao', right_on='airport_ident', how='left')
+            .drop(columns=['id', 'airport_ident', 'airport_ref'])
+            .dropna(axis=0)
+            .reset_index()
+         )
+
+        return df_all
+
+
+# =========================================================================================
+# =========================================================================================
+# IMPORT FUNCTIONS
+# =========================================================================================
+# =========================================================================================
+
 
 def import_airports(db):
     airport_data = AirportLoader().get_airport_data()
-    pbar = tqdm(total = len(airport_data), desc="Creating entries")
-    list_airports = []
+    pbar = tqdm(total = len(airport_data), desc="Creating Airports")
     for i, airport in airport_data.iterrows():
         new_airport = Airport(
             name = airport['name'],
@@ -144,8 +247,7 @@ def import_airports(db):
 
 def import_runways(db):
     runway_data = RunwayLoader().get_runway_data()
-    pbar = tqdm(total = len(runway_data), desc="Creating entries")
-    list_runways = []
+    pbar = tqdm(total = len(runway_data), desc="Creating Runways")
     for i, runway in runway_data.iterrows():
         # this_airport = Airport.query.filter(Airport.icao == runway['icao']).first()
         # if this_airport == None:
@@ -177,6 +279,57 @@ def import_runways(db):
     print("Done !")
 
 
+def import_navaids(db):
+    navaid_data = NavaidLoader().get_navaid_data()
+    pbar = tqdm(total = len(navaid_data), desc="Creating Navaids")
+    for i, navaid in navaid_data.iterrows():
+        new_navaid = Navaid(
+            id = i,
+            ident = navaid['ident'],
+            name = navaid['navaid_name'],
+            nav_type = navaid['type'],
+            frequency = navaid['frequency_khz'],
+            latitude = navaid['latitude_deg'],
+            longitude = navaid['longitude_deg'],
+            altitude = navaid['elevation_ft'],
+            usage = navaid['usageType'],
+            power = navaid['power'],
+            airport = navaid['airport_icao'],
+            country = navaid['country'],
+        )
+        db.session.add(new_navaid)
+        db.session.commit()
+        pbar.update(1)
+    pbar.close()
+
+    print("Done !")
+
+
+
+
+def import_frequencies(db):
+    frequency_data = FrequencyLoader().get_frequency_data()
+    pbar = tqdm(total = len(frequency_data), desc="Creating Frequencies")
+    for i, frequency in frequency_data.iterrows():
+        new_frequency = Frequency(
+            id = i,
+            airport = frequency['icao'],
+            frq_type = frequency['type'],
+            desc = frequency['description'],
+            frq_mhz = frequency['frequency_mhz'],
+        )
+        db.session.add(new_frequency)
+        db.session.commit()
+        pbar.update(1)
+    pbar.close()
+
+    print("Done !")
+
+
+
+# =========================================================================================
+# =========================================================================================
+# =========================================================================================
 
 
 def init_db():
@@ -184,5 +337,10 @@ def init_db():
     db.create_all()
     import_airports(db)
     import_runways(db)
+    import_navaids(db)
+    import_frequencies(db)
     lg.warning('Database initialized!')
 
+
+if __name__ == '__main__':
+    init_db()
--- tests/app_flask/api_app/models.py
+++ tests/app_flask/api_app/models.py
@@ -73,3 +73,56 @@ class Runway(db.Model):
 
     def __str__(self):
         return f"(Runway) : {self.le_ident}/{self.he_ident} - {self.airport}"
+
+
+
+class Navaid(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ident = db.Column(db.String(10), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    nav_type = db.Column(db.String(10), nullable=False)
+    frequency = db.Column(db.Integer, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+    altitude = db.Column(db.Float, nullable=False)
+    usage = db.Column(db.String(10), nullable=True)
+    power = db.Column(db.String(10), nullable=True)
+    airport = db.Column(db.String(4), db.ForeignKey('airport.icao'), nullable=True)
+    country = db.Column(db.String(50), nullable=False)
+
+    def __init__(self, id, ident, name, nav_type, frequency,
+                    latitude, longitude, altitude,
+                    usage, power, airport, country):
+        self.id = id
+        self.ident = ident
+        self.name = name
+        self.nav_type = nav_type
+        self.frequency = frequency
+        self.latitude = latitude
+        self.longitude = longitude
+        self.altitude = altitude
+        self.usage = usage
+        self.power = power
+        self.airport = airport
+        self.country = country
+    
+    def __str__(self):
+        return f"(Navaid) : {self.name} - {self.frequency}"
+
+
+class Frequency(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    airport = db.Column(db.String(4), db.ForeignKey('airport.icao'), nullable=True)
+    frq_type = db.Column(db.String(50), nullable=False)
+    desc = db.Column(db.String(100), nullable=False)
+    frq_mhz = db.Column(db.Float, nullable=False)
+
+    def __init__(self, id, airport, frq_type, desc, frq_mhz):
+        self.id = id
+        self.airport = airport
+        self.frq_type = frq_type
+        self.desc = desc
+        self.frq_mhz = frq_mhz
+
+    def __str__(self):
+        return f"(Frequency) : {self.airport} {self.frq_type} - {self.frq_mhz}"
